fe4b0c9cdcfa8fd36452505f13ee4b9bf048159b
Ole Martin Pettersen <olemp@puzzlepart.com>
2017-09-20 08:43:57 +0200

    Update

--- src/webparts/newsAdmin/INewsAdminWebPartProps.ts
+++
@@ -1,3 +0,0 @@
-export interface INewsAdminWebPartProps {
-  description: string;
-}
--- src/webparts/newsAdmin/test/NewsAdminWebPart.test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mocha" />
-
-import { assert } from 'chai';
-
-describe('NewsAdminWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
